6697efe00ec7dff8719f2164be182a987bd769c6
Hansen Taidy <hansen@taidy.me>
2024-01-10 16:20:54 +0700

    Add help message and move markComplete deleteTodo to TodoView.vue from TodoItem.vue

---
+++ src/components/HelpMessage.vue
@@ -0,0 +1,26 @@
+<template>
+  <article class="message is-warning mb-2">
+    <div class="message-header">
+      <p>How to use</p>
+      <button class="delete" aria-label="delete" @click="hide"></button>
+    </div>
+    <div class="message-body content is-small">
+      <ul>
+        <li>Fill in the empty field, then hit enter to add a new todo.</li>
+        <li>Click on 'All', 'Incomplete', 'Completed' to filter the list.</li>
+        <li>Click on the item to mark as completed.</li>
+        <li>Right click on the item to delete.</li>
+      </ul>
+    </div>
+  </article>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits({
+  hidden: String
+});
+
+function hide() {
+  emit('hidden');
+}
+</script>
--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,16 +1,24 @@
 <template>
-  <main class="container my-5">
-    <div class="columns is-mobile is-centered">
+  <section class="container my-5">
+    <div class="columns is-centered">
       <div class="column is-half">
-        <article class="panel is-link">
-          <p class="panel-heading">Todo</p>
+        <article class="panel is-link ">
+          <p class="panel-heading">
+            Vue 3 Simple Todo App
+            <span class="is-pulled-right is-clickable" v-if="!isMessageShown" @click="hideMessage">
+              <i class="fa fa-info-circle"></i>
+            </span>
+          </p>
+          <Transition name="bounce">
+            <HelpMessage v-if="isMessageShown" @hidden="hideMessage" />
+          </Transition>
           <p class="panel-tabs">
             <a
-              v-for="(f, i) in filters"
-              :key="i"
-              @click="todosStore.filterTodo(i)"
-              :class="{ 'is-active': i === todosStore.filter }"
-              >{{ f }}</a
+                v-for="(f, i) in filters"
+                :key="i"
+                @click="todosStore.filterTodo(i)"
+                :class="{ 'is-active': i === todosStore.filter }"
+            >{{ f }}</a
             >
           </p>
           <div class="panel-block">
@@ -23,25 +31,29 @@
           </div>
           <TransitionGroup name="list">
             <TodoItem
-              v-for="(todo, index) in filteredTodos"
-              :key="todo.id"
-              :todo="todo"
-              :index="index"
+                v-for="todo in filteredTodos"
+                :key="todo.id"
+                :todo="todo"
+                @click="todosStore.markComplete(todo)"
+                @click.right.prevent="todosStore.deleteTodo(todo)"
             />
+            <!--  -->
           </TransitionGroup>
         </article>
       </div>
     </div>
-  </main>
+  </section>
   <FooterLayout />
 </template>
 
 <script setup lang="ts">
-import TodoInput from '@/components/TodoInput.vue'
-import { storeToRefs } from 'pinia'
-import { useTodos } from '@/stores/todos'
-import TodoItem from '@/components/TodoItem.vue'
-import FooterLayout from '@/components/FooterLayout.vue'
+import TodoInput from '@/components/TodoInput.vue';
+import { storeToRefs } from 'pinia';
+import { useTodos } from '@/stores/todos';
+import TodoItem from '@/components/TodoItem.vue';
+import HelpMessage from '@/components/HelpMessage.vue';
+import FooterLayout from '@/components/FooterLayout.vue';
+import { ref } from 'vue';
 
 // const TodoAll = defineAsyncComponent(() => import('@/components/TodoAll.vue'))
 // const TodoCompleted = defineAsyncComponent(() => import('@/components/TodoCompleted.vue'))
@@ -53,12 +65,17 @@ import FooterLayout from '@/components/FooterLayout.vue'
 //   TodoIncomplete
 // })
 
+const isMessageShown = ref(true);
+
+function hideMessage() {
+  isMessageShown.value = !isMessageShown.value;
+}
+
 const filters = {
   all: 'All',
   incomplete: 'Incomplete',
   completed: 'Completed'
-}
-const todosStore = useTodos()
-const { filteredTodos } = storeToRefs(todosStore)
-
+};
+const todosStore = useTodos();
+const { filteredTodos } = storeToRefs(todosStore);
 </script>
